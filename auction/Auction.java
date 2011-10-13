a2cc84db1d1ef3075f0f09241f7c458f560e2a64
Brian Becker <muckwaddle@gmail.com>
2011-10-13 13:42:41 -0500

    Added comments and implement Bid type. x2

--- auction/Auction.java
+++ auction/Auction.java
@@ -2,7 +2,19 @@ package auction;
 
 public interface Auction {
     
-    public Range<Bid> getValidBid();
+    /**
+     *  Gets a range of valid bids for this particular auction.
+     *
+     *  @ensure: getValidBids().contains(i) for all i which is a valid
+     *      bid.
+    */
+    public Range<Bid> getValidBids();
+    
+    /**
+     *  Places a bid on this auction.
+     *
+     *  @require: getValidBids().contains(bid)
+    */
     public placeBid(Bid bid);
     
 }
