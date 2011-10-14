e5cb20ba6ffc2bf9190a4531acbd5823ab417d78
Brian Becker <muckwaddle@gmail.com>
2011-10-14 14:05:56 -0500

    Rename ascending auction into main auction package.

--- src/us/nstro/javaauction/auction/AscendingAuction.java
+++ src/us/nstro/javaauction/auction/AscendingAuction.java
@@ -1,33 +1,28 @@
-package us.nstro.javaauction.auction.ascending;
+package us.nstro.javaauction.auction;
 
 import us.nstro.javaauction.types.selection.Selection;
-import us.nstro.javaauction.auction.Price;
-import us.nstro.javaauction.auction.Bid;
-import us.nstro.javaauction.auction.AbstractAuction;
-import us.nstro.javaauction.auction.AuctionInfo;
-import us.nstro.javaauction.auction.User;
 
 /**
  *  The AscendingAuction implements an Auction which is a traditional open
  *  ascending auction, like E-Bay.
-*/
+ */
 public class AscendingAuction extends AbstractAuction {
-    
-    private Selection<Price> validPrices;
     
     /**
      *  Creates a new Ascending auction with a minimum bid of min.
-    */
+     */
     public AscendingAuction(AuctionInfo info, Price min) {
         super(info);
         this.updateValidPrices(new Selection<Price>(min));
     }
     
     /**
-     *  Places the bid 'bid' on this auction. After bidding, the
+     * Places the bid 'bid' on this auction. After bidding, the ascending
+     * auction is defined to accept the bid so long as it is higher than any
+     * other bid.
      *
-     *  @require: getValidBids().contains(bid)
-    */
+     * @require: getValidBids().contains(bid)
+     */
     public void placeBid(Bid bid) {
         this.setCurrentWinningBid(bid);
         this.updateValidPrices(new Selection<Price>(bid.getPrice().next()));
