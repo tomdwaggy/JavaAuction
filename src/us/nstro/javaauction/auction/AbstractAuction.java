2cbab9701d880ff0f700fc51915dfc6f5056c7a0
Brian Becker <muckwaddle@gmail.com>
2011-10-14 12:04:11 -0500

    Add status to Auction, AbstractAuction. Implement Auction by AbstractAuction.

--- src/us/nstro/javaauction/auction/AbstractAuction.java
+++ src/us/nstro/javaauction/auction/AbstractAuction.java
@@ -9,15 +9,17 @@ package us.nstro.javaauction.auction;
  *
  * @author bbecker
  */
-public abstract class AbstractAuction {
+public abstract class AbstractAuction implements Auction {
 
     private AuctionInfo info;
+    private AuctionStatus status;
 
     /**
      * Create a new Abstract Auction.
      */
     public AbstractAuction(AuctionInfo info) {
         this.info = info;
+        this.status = new AuctionStatus();
     }
 
     /**
@@ -25,6 +27,13 @@ public abstract class AbstractAuction {
      */
     public AuctionInfo getInfo() {
         return this.info;
+    }
+
+    /**
+     * Get the auction information.
+     */
+    public AuctionStatus getStatus() {
+        return this.status;
     }
 
 
