524a91bed6350fe9d96966e78391c9cff1cf8e10
Brian Becker <muckwaddle@gmail.com>
2011-10-14 17:35:48 -0500

    Add some more documentation to AbstractAuction.

--- src/us/nstro/javaauction/auction/Auction.java
+++ src/us/nstro/javaauction/auction/Auction.java
@@ -9,37 +9,53 @@ import us.nstro.javaauction.types.selection.Selection;
 public interface Auction {
 
     /**
-     * Gets the auction information.
+     * Get the auction information. This is static information about an
+     * auction, such as who the auctioneer is, what is being auctioned off,
+     * the auction name and ID, etc.
+     *
+     * @ensure: getInfo() != null
      */
     public AuctionInfo getInfo();
 
     /**
-     * Gets the auction status.
+     * Gets the auction status. This is dynamic data, such as whether the
+     * auction is closed or not, and who the winner is (if closed).
+     *
+     * @ensure: getInfo() != null
      */
     public AuctionStatus getStatus();
 
     /**
-     *  Gets a selection of valid bid prices for this auction.
+     * Gets a selection of valid bid prices for this auction.
      *
-     *  @ensure: getValidBids().contains(i) for all i which is a valid
-     *      bid.
+     * @require: getStatus.isClosed() == false
+     * @ensure: getValidBids().contains(i) for all i which is a valid
+     *     bid.
     */
     public Selection<Price> getValidPrices();
     
     /**
      *  Places the bid 'bid' on this auction. After bidding, the
      *
-     *  @require: getValidBids().contains(bid)
+     *  @require: getStatus.isClosed() == false &&
+     *      getValidBids().contains(bid)
     */
     public void placeBid(Bid bid);
 
     /**
      * Closes the auction normally, committing the winning bid.
+     *
+     * @require: getSTatus().isClosed() == false
+     * @ensure: getStatus().isClosed() == true
      */
     public void closeAuction();
 
     /**
      * Aborts the auction, not committing any current winners.
+     *
+     * @require: getStatus().isClosed() == false
+     * @ensure: getStatus().isClosed() == true &&
+     *      getStatus().hasWinner() == false
      */
     public void abortAuction();
     
